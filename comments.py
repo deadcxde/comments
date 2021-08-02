27e9260612f6e786fc9cc22e73f5a8132bcd8f3e
insan1tyyy <71565965+insan1tyyy@users.noreply.github.com>
2021-08-02 13:49:58 +0300

    Add files via upload

--- comments.py
+++ comments.py
@@ -1,4 +1,4 @@
-APP_VERSION = 0.1
+APP_VERSION = 0.11
 import vk_api
 from requests import post, get
 import time
@@ -13,7 +13,6 @@ def installUpdate():
         f.write(r)
     print("Обновление успешно установлено! Запусти скрипт заново.")
     exit()
-    return
 
 def checkUpdates():
     r:str = get('https://raw.githubusercontent.com/insan1tyyy/comments/main/comments.py').text
@@ -32,26 +31,26 @@ checkUpdates()
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-def login() -> str:
+def login():
     while True:
-        token = input("Введи токен: ")
-        try:
-            user = vk_api.VkApi(token=token).get_api().users.get()[0]
-            name = '{} {}'.format(user['first_name'], user['last_name'])
-            print(f'Успешная авторизация как {name} !')
-            return token
+        token = input("Введи токен от страницы. Если хочешь использовать несколько токенов, впиши их, разделив знаком ;\n\n>>> ")
+        if ';' in token:
+            tokens = token.replace(' ', '').split(';')
+        else:
+            try:
+                user = vk_api.VkApi(token=token).get_api().users.get()[0]
+                name = '{} {}'.format(user['first_name'], user['last_name'])
+                print(f'Успешная авторизация как {name} !')
+                return token
 
-        except vk_api.exceptions.VkApiError:
-            print('Неверный токен.')
-            continue
+            except vk_api.exceptions.VkApiError:
+                print('Неверный токен.')
+                continue
 
 if not config['token']:
     config['token'] = login()
     with open('config.json', 'w') as f: 
         json.dump(config, f, indent = 4)
-
-vk = vk_api.VkApi(token=config['token']).get_api()
-
 
 def newComments() -> list:
     comments = input("Введи новый текст для комментов. Чтобы разделить текст, поставь ; (Пример: Hello world; Bye world; i want some candies)\n\n>>> ").split(';')
@@ -67,6 +66,17 @@ def answerComments():
     else:
         return
 
+def answerTokens():
+    a = input("Если хочешь вставить новые токены, введи любой символ, иначе нажми ENTER, оставив поле пустым.\n\n>>> ")
+    if a:
+        config['token'] = login()
+        with open('config.json', 'w') as f:
+            json.dump(config, f, indent = 4)
+        return
+    else:
+        return
+
+
 if not config['comments']:
     config['comments'] = newComments()
     with open('config.json', 'w') as f:
@@ -74,19 +84,49 @@ if not config['comments']:
 else:
     answerComments()
 
-post_id = vk.wall.get()['items'][0]['id']
-
-start = time.time()
-commentList = config['comments']
-print('\n\nНачинаю накрутку...')
-while True:
-    msg = choice(commentList)
-    try:
-        vk.wall.createComment(post_id = post_id, message = msg)
-        print(f'[' + str(round(time.time() - start, 3)) + f'] Comment added: "{msg}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
-    except vk_api.exceptions.Captcha as e:
-        print(f'[' + str(round(time.time() - start, 3)) + f'] Comment error: CAPTCHA. trying to sleep 300s...')
-        time.sleep(300)
+def multiTokenSupport():
     
+    post_id = vk_api.VkApi(token=config['token'][0]).get_api().wall.get()['items'][0]['id']
+    user_id = vk_api.VkApi(token=config['token'][0]).get_api().users.get()[0]['id']
+    start = time.time()
+    commentList = config['comments']
+    print('\n\nНачинаю накрутку...')
+    while True:
+        for token in config['token']:
+            vk = vk_api.VkApi(token=token).get_api()
+            msg = choice(commentList)
+            try:
+                vk.wall.createComment(post_id = post_id, message = msg, owner_id = user_id)
+                print(f'[' + str(round(time.time() - start, 3)) + f'] Comment added: "{msg}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
+            except vk_api.exceptions.Captcha as e:
+                print(f'[' + str(round(time.time() - start, 3)) + f'] Comment error: CAPTCHA. skip to next token...')
+                continue
+
+def singleTokenSupport():
+    vk = vk_api.VkApi(token=config['token']).get_api()
+    post_id = vk.wall.get()['items'][0]['id']
+
+    start = time.time()
+    commentList = config['comments']
+    print('\n\nНачинаю накрутку...')
+    while True:
+        msg = choice(commentList)
+        try:
+            vk.wall.createComment(post_id = post_id, message = msg)
+            print(f'[' + str(round(time.time() - start, 3)) + f'] Comment added: "{msg}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
+        except vk_api.exceptions.Captcha as e:
+            print(f'[' + str(round(time.time() - start, 3)) + f'] Comment error: CAPTCHA. trying to sleep 300s...')
+            time.sleep(300)
+
+def startFunc():
+    if type(config['token']) == type([1,2,3]):
+        multiTokenSupport()
+    else:
+        singleTokenSupport()
+
+
+startFunc()
+
+
 
 
