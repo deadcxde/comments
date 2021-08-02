75af8c89702278544a1c175d022f78ea34e184b3
insan1tyyy <71565965+insan1tyyy@users.noreply.github.com>
2021-08-02 14:13:30 +0300

    Add files via upload

--- comments.py
+++ comments.py
@@ -1,17 +1,18 @@
-APP_VERSION = 0.12
+APP_VERSION = 0.13
 import vk_api
 from requests import post, get
 import time
 import json
 import sys
 from random import choice
-
+from colorama import init, Fore
+init()
 def installUpdate():
     r = get('https://raw.githubusercontent.com/insan1tyyy/comments/main/comments.py').text
     r = r.replace('\r', '')
     with open('comments.py', 'w', encoding='utf-8') as f:
         f.write(r)
-    print("Обновление успешно установлено! Запусти скрипт заново.")
+    print(f"{Fore.CYAN}Обновление успешно установлено! Запусти скрипт заново.")
     exit()
 
 def checkUpdates():
@@ -36,15 +37,34 @@ def login():
         token = input("Введи токен от страницы. Если хочешь использовать несколько токенов, впиши их, разделив знаком ;\n\n>>> ")
         if ';' in token:
             tokens = token.replace(' ', '').split(';')
+            success = []
+            for token in tokens:
+                try:
+                    vk = vk_api.VkApi(token=token).get_api()
+                    vk.wall.get()
+                    success.append(token)
+                except vk_api.exceptions.VkApiError as e:
+                    if e.error['error_msg'] == 'Group authorization failed: method is unavailable with group auth.':
+                        pass
+                    else:
+                        print(f'{Fore.RED}Неверный токен. {token}')
+                        continue
+                try:
+                    user = vk.users.get()[0]
+                    name = '{} {}'.format(user['first_name'], user['last_name'])
+                    print(f'{Fore.YELLOW} Успешная авторизация как {name} !\n')
+                except IndexError:
+                    print(f'{Fore.YELLOW} Успешная авторизация токеном паблика {token}')
+            return success
         else:
             try:
                 user = vk_api.VkApi(token=token).get_api().users.get()[0]
                 name = '{} {}'.format(user['first_name'], user['last_name'])
-                print(f'Успешная авторизация как {name} !')
+                print(f'{Fore.YELLOW} Успешная авторизация как {name} !')
                 return token
 
             except vk_api.exceptions.VkApiError:
-                print('Неверный токен.')
+                print(f'{Fore.RED}Неверный токен.')
                 continue
 
 def newComments() -> list:
@@ -94,16 +114,16 @@ def multiTokenSupport():
     user_id = vk_api.VkApi(token=config['token'][0]).get_api().users.get()[0]['id']
     start = time.time()
     commentList = config['comments']
-    print('\n\nНачинаю накрутку...')
+    print(f'\n\n{Fore.MAGENTA}Начинаю накрутку...')
     while True:
         for token in config['token']:
             vk = vk_api.VkApi(token=token).get_api()
             msg = choice(commentList)
             try:
                 vk.wall.createComment(post_id = post_id, message = msg, owner_id = user_id)
-                print(f'[' + str(round(time.time() - start, 3)) + f'] Comment added: "{msg}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
+                print(f'{Fore.GREEN}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.GREEN}] Comment added: "{msg}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
             except vk_api.exceptions.Captcha as e:
-                print(f'[' + str(round(time.time() - start, 3)) + f'] Comment error: CAPTCHA. skip to next token...')
+                print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: CAPTCHA. skip to next token...')
                 continue
 
 def singleTokenSupport():
@@ -112,14 +132,14 @@ def singleTokenSupport():
 
     start = time.time()
     commentList = config['comments']
-    print('\n\nНачинаю накрутку...')
+    print(f'\n\n{Fore.MAGENTA}Начинаю накрутку...')
     while True:
         msg = choice(commentList)
         try:
             vk.wall.createComment(post_id = post_id, message = msg)
-            print(f'[' + str(round(time.time() - start, 3)) + f'] Comment added: "{msg}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
+            print(f'{Fore.GREEN}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.GREEN}] Comment added: "{Fore.CYAN}{msg}{Fore.GREEN}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
         except vk_api.exceptions.Captcha as e:
-            print(f'[' + str(round(time.time() - start, 3)) + f'] Comment error: CAPTCHA. trying to sleep 300s...')
+            print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: CAPTCHA. trying to sleep 300s...')
             time.sleep(300)
 
 def startFunc():
