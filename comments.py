cf6f659d0d4bb7e1619e1eceec5f3572973cd7cb
insan1tyyy <71565965+insan1tyyy@users.noreply.github.com>
2021-08-03 08:31:25 +0300

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,9 +1,11 @@
-APP_VERSION = 0.15
+APP_VERSION = 0.16
 import vk_api
 from requests import post, get
 import time
 import json
 import sys
+from vk_api import upload
+from vk_api.longpoll import VkLongPoll, VkEventType
 from random import choice
 from colorama import init, Fore
 init()
@@ -65,6 +67,17 @@ def checkUpdateNotification():
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+def newCaptchaUserId() -> int:
+    userId = input("Чтобы пропустить этот этап - оставь поле пустым.\nВведи ссылку на пользователя, кому отправлять капчу\nОбработка капчи работает только в режиме одного токена!!\n>>> ").split('.com/')[1]
+    if not userId or userId == '0':
+        return 0
+    if type(config['token']) == type([1,2,3]):
+        token = config['token'][0]
+    else:
+        token = config['token']
+    return vk_api.VkApi(token=token).get_api().users.get(user_ids=userId)[0]['id']
+
+
 def login():
     while True:
         token = input("Введи токен от страницы. Если хочешь использовать несколько токенов, впиши их, разделив знаком ;\n\n>>> ")
@@ -114,6 +127,16 @@ def answerComments():
     else:
         return
 
+def answerCaptcha():
+    a = input("Если хочешь вставить новый АЙДИ чата для капчи, введи любой символ, иначе нажми ENTER, оставив поле пустым.\n\n>>> ")
+    if a:
+        config['captchaTo'] = newCaptchaUserId()
+        with open('config.json', 'w') as f:
+            json.dump(config, f, indent = 4)
+        return
+    else:
+        return
+
 def answerTokens():
     a = input("Если хочешь вставить новые токены, введи любой символ, иначе нажми ENTER, оставив поле пустым.\n\n>>> ")
     if a:
@@ -137,6 +160,38 @@ if not config['comments']:
         json.dump(config, f, indent = 4)
 else:
     answerComments()
+
+if not config.get('captchaTo', None):
+    config['captchaTo'] = newCaptchaUserId()
+    with open('config.json', 'w') as f:
+        json.dump(config, f, indent = 4)
+else:
+    answerCaptcha()
+
+def sendCaptcha(vk, captcha):
+    r = open("captcha.jpg", "wb")
+    r.write(get(captcha.url).content)
+    r.close()
+    load = upload.VkUpload(vk)
+    captchaPhoto = load.photo_messages(peer_id=config['captchaTo'], photos='captcha.jpg') 
+    vk.messages.send(peer_id = config['captchaTo'], random_id = 0, message = 'Отправьте код с картинки следующим образом:\n!get 123code', attachment = 'photo' + str(captchaPhoto[0]['owner_id']) + '_' + str(captchaPhoto[0]['id']))
+    return
+
+def processCaptcha(token, captcha):
+    vk_session = vk_api.VkApi(token=token)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+    sendCaptcha(vk, captcha)
+    startCaptcha = int(time.time())
+    while True:
+        for event in longpoll.listen():
+            if time.time() - startCaptcha == 900:
+                print("Ответа на капчу не было получено.")
+                return
+            if event.type == VkEventType.MESSAGE_NEW:
+                if '!c' in event.text and not event.from_me:
+                    print("Ответ на капчу принят, код:" + event.text.replace('!get ', ''))
+                    return captcha.try_again(event.text.replace('!get ', ''))
 
 
 
@@ -164,30 +219,57 @@ def multiTokenSupport():
             try:
                 vk.wall.createComment(post_id = post_id, message = msg, owner_id = user_id)
                 print(f'{Fore.GREEN}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.GREEN}] Comment added: "{msg}" comment count: ' + str(vk.wall.get(owner_id = user_id)['items'][0]['comments']['count']))
-            except vk_api.exceptions.Captcha as e:
-                print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: CAPTCHA. set {TOKEN_DELAY}s delay...')
+            except vk_api.exceptions.Captcha as captcha:
                 
+                # captchaTries += 1
+                if captchaTries < 3:
+                    captchaTries = 0
+                    print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: CAPTCHA. wait captcha answer...')
+                    processCaptcha(token)
+                    pass
+                else:
+                    print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: CAPTCHA. set {TOKEN_DELAY}s delay...')
                 data['tokensOnDelay'].append(token)
                 data['tokenDelays'][token] = int(time.time())
                 dumpData(data)
                 continue
+            except vk_api.exceptions.ApiError as e:
+                print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: ' + e.error['error_msg'] + f'. trying to sleep 300s... (token: {token})')
 
 def singleTokenSupport():
+
+
     vk = vk_api.VkApi(token=config['token']).get_api()
     post_id = vk.wall.get()['items'][0]['id']
 
     start = time.time()
     commentList = config['comments']
     print(f'\n\n{Fore.MAGENTA}Начинаю накрутку...')
+    captchaTries = 0
     while True:
         checkUpdateNotification()
         msg = choice(commentList)
         try:
             vk.wall.createComment(post_id = post_id, message = msg)
+            captchaTries = 0
             print(f'{Fore.GREEN}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.GREEN}] Comment added: "{Fore.CYAN}{msg}{Fore.GREEN}" comment count: ' + str(vk.wall.get()['items'][0]['comments']['count']))
-        except vk_api.exceptions.Captcha as e:
+        except vk_api.exceptions.Captcha as captcha:
+            if not config['captchaTo'] or config['captchaTo'] == '0':
+                print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: CAPTCHA. trying to sleep 300s...')
+                time.sleep(300)
+                continue
+            captchaTries += 1
+            if captchaTries >= 3:
+                print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: Капча отправлена в выбранный чат, ожидаю ответа 900сек...')
+                if not processCaptcha(config['token'], captcha):
+                    continue
+                else:
+                    captchaTries = 0
+                    continue
             print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: CAPTCHA. trying to sleep 300s...')
             time.sleep(300)
+        except vk_api.exceptions.ApiError as e:
+            print(f'{Fore.RED}[{Fore.YELLOW}' + str(round(time.time() - start, 3)) + f'{Fore.RED}] Comment error: ' + e.error['error_msg'] + f'. trying to sleep 300s...')
 
 def startFunc():
     if type(config['token']) == type([1,2,3]):
